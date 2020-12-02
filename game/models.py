dbf05bba035f7791cc07affbad869be69a144e8c
patrick bachi <patrick.bachi@themrggroup.com>
2020-12-02 14:44:23 -0300

    Category Model Metadata

--- game/models.py
+++ game/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "Categories"
     
     def __str__(self):
         return self.title
