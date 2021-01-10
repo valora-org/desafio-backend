964684321bbf4fe4118b0c6f8c421f22ffac54b5
alberto <alberto.oliveira@ccc.ufcg.edu.br>
2021-01-10 20:00:16 -0300

    Scrip para alimenta o banco com categirias & questões

--- valora/quiz/scrip_add_question.py
+++ valora/quiz/scrip_add_question.py
@@ -4,12 +4,17 @@ from quiz.models import Question, Category
 from valora.settings import BASE_DIR
 
 
+# */Roda script no python console para alimenta o banco de questões
+# script :
+# from quiz import scrip_add_question
+# alimentar_banco()
+# *
+
 def alimentar_banco():
     Category.objects.get_or_create(category="Matemática")
     Category.objects.get_or_create(category="Python")
     arq_xls = str(BASE_DIR.absolute()) + '/tabela_quiz.xls'
     xls = xlrd.open_workbook(arq_xls)
-
     plan = xls.sheets()[0]
     for i in range(1, plan.nrows):
         category = Category.objects.get(pk=int(plan.row_values(i)[0]))
