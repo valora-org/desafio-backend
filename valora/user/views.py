94ab4be93fefa48a2172ad1a4a4efed1e5c8d1ce
alberto <alberto.oliveira@ccc.ufcg.edu.br>
2021-01-10 20:16:11 -0300

    Reorganizando imports

--- valora/user/views.py
+++ valora/user/views.py
@@ -1,64 +1,61 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
-from rest_framework import permissions
-from rest_framework.response import Response
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework import permissions, response, viewsets
 
-from user.models import UserProfile
-from .serializers import UserProfileSerializer, TempUserProfileSerializer
+from user import models, serializers
 
 ID_FAKE = 0
 
 
-class BasicList(ReadOnlyModelViewSet):
+class BasicList(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
-        return UserProfile.objects.filter(user__is_superuser=False).order_by('-total_points').distinct()
+        return models.UserProfile.objects.filter(user__is_superuser=False).order_by('-total_points').distinct()
 
     def get_category__id(self):
         return ID_FAKE
 
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
-        UserProfile.att_sequential(queryset)
-        UserProfile.att_pontuacao(self.get_category__id(), category=False)
+        models.UserProfile.att_sequential(queryset)
+        models.UserProfile.att_pontuacao(self.get_category__id(), category=False)
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
 
         serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+        return response.Response(serializer.data)
 
 
 class RankingAPIView(BasicList):
-    queryset = UserProfile.objects.all().order_by('-sequential')
-    serializer_class = UserProfileSerializer
+    queryset = models.UserProfile.objects.all().order_by('-sequential')
+    serializer_class = serializers.UserProfileSerializer
     filterset_fields = ['user']
     permission_classes = (permissions.IsAuthenticated,)
 
 
 class RankingCategoryAPIView(BasicList):
-    queryset = UserProfile.objects.all().order_by('-sequential')
-    serializer_class = UserProfileSerializer
+    queryset = models.UserProfile.objects.all().order_by('-sequential')
+    serializer_class = serializers.UserProfileSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
     def get_category__id(self):
         return self.kwargs['category_id']
 
     def get_queryset(self):
-        return UserProfile.objects.filter(user__is_superuser=False,
+        return models.UserProfile.objects.filter(user__is_superuser=False,
                                           question_correct__category__id=
                                           self.get_category__id()).order_by('-points').distinct()
 
 
-class TempRankingAPIView(ReadOnlyModelViewSet):
-    queryset = UserProfile.objects.filter().order_by('-points')
-    serializer_class = TempUserProfileSerializer
+class TempRankingAPIView(viewsets.ReadOnlyModelViewSet):
+    queryset = models.UserProfile.objects.filter().order_by('-points')
+    serializer_class = serializers.TempUserProfileSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self):
-        return UserProfile.objects.filter(user=self.request.user)
+        return models.UserProfile.objects.filter(user=self.request.user)
 
 
 @login_required
