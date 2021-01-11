407e6dc34faaf2b542afce2e34c9bb823cec2561
alberto <alberto.oliveira@ccc.ufcg.edu.br>
2021-01-10 23:19:22 -0300

    ajuste admin user

--- valora/user/apps.py
+++ valora/user/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+from import_export.admin import ImportExportModelAdmin
 
 
 class UserConfig(AppConfig):
     name = 'user'
+
+
+class ProfileAdmin(ImportExportModelAdmin):
+    raw_id_fields = ('user',)
+    list_display = ('user', 'points',)
