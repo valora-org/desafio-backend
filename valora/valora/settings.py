760a359dcff653fed4665d1cb4b547206366e3cd
alberto <alberto.oliveira@ccc.ufcg.edu.br>
2021-01-10 23:21:15 -0300

    add Application

--- valora/valora/settings.py
+++ valora/valora/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'django.contrib.sites',
     'rest_auth.registration',
+    'import_export',
 
 
     #APPS
@@ -79,15 +80,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         # 'rest_framework.authentication.BasicAuthentication'
-        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication', # para teste
 
-        # <-- And here
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.AdminRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework.renderers.TemplateHTMLRenderer',
+
     ],
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
