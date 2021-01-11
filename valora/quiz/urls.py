3e432eb47311aca65bc448613ead891e618dd0f0
alberto <alberto.oliveira@ccc.ufcg.edu.br>
2021-01-10 23:23:30 -0300

    ajustes urls

--- valora/quiz/urls.py
+++ valora/quiz/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('category_adm/', views.CategoryAdminViewSet.as_view({'get': 'list'}), name='category-adm'),
     path('category_list/', views.CategoryViewSet.as_view({'get': 'list'}), name='category_list'),
     path('question_adm/', views.QuestionAdminViewSet.as_view({'get': 'list'}), name='question-adm'),
-    path('<int:category_id>/', views.StartQuiz.as_view(), name='start'),
+    path('<int:category_id>/', views.StartQuiz.as_view({'get': 'list'}), name='start'),
 ]
--- valora/valora/urls.py
+++ valora/valora/urls.py
@@ -22,8 +22,8 @@ from quiz.views import CategoryAdminViewSet, QuestionAdminViewSet, CategoryViewS
 from user.views import RankingAPIView
 
 api_router = routers.DefaultRouter()
-api_router.register(r"category", CategoryAdminViewSet )
-api_router.register(r"question", QuestionAdminViewSet )
+api_router.register(r"category", CategoryAdminViewSet)
+api_router.register(r"question", QuestionAdminViewSet)
 api_router.register(r"category_list", CategoryViewSet, basename='category-list')
 api_router.register(r"ranking", RankingAPIView, basename='ranking')
 
@@ -32,10 +32,10 @@ urlpatterns = [
     path('user/', include('user.urls')),
     path('quiz/', include('quiz.urls')),
     path('', include(api_router.urls)),
-    path('api-auth/', include('rest_framework.urls')), # Para Teste
+    path('api-auth/', include('rest_framework.urls')),# Para Teste
     path('rest_auth/', include('rest_auth.urls')),
     path('rest_auth/registration/',include('rest_auth.registration.urls')),
-    path("api-token-auth/", obtain_auth_token, name="api_token_auth"), # Para Teste
+    path("api-token-auth/", obtain_auth_token, name="api_token_auth"),# Para Teste
 
 
 ]
