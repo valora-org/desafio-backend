937061edf9493f30b99c83cfe413b567fbee3acf
alberto <alberto.oliveira@ccc.ufcg.edu.br>
2021-01-10 23:18:48 -0300

    ajuste admin user

--- valora/user/admin.py
+++ valora/user/admin.py
@@ -3,13 +3,11 @@ from import_export.admin import ImportExportModelAdmin
 
 from user.models import UserProfile
 
+
 # Register your models here.
 
 
-admin.site.register(UserProfile)
-
+@admin.register(UserProfile)
 class ProfileAdmin(ImportExportModelAdmin):
     raw_id_fields = ('user',)
     list_display = ('user', 'points',)
-
-
