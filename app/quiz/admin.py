be1f8e40b489ac65940e29965429c7df1870a845
deilsonmartins <deilsonengeletrica@gmail.com>
2021-01-15 18:06:37 -0300

    criação do admin de quiz

--- app/quiz/admin.py
+++ app/quiz/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+import nested_admin
+from .models import Quiz, Question, Answer, SubmitPlayer, PlayersAnswer
 
-# Register your models here.
+
+class AnswerInline(nested_admin.NestedTabularInline):
+	"""
+		Setting the number of answers for registration for single question.
+
+	"""
+	model = Answer
+	extra = 3
+	max_num = 3
+
+
+class QuestionInline(nested_admin.NestedTabularInline):
+	"""
+		Setting the number of questions for registration for single quiz.
+
+	"""
+	model = Question
+	inlines = [AnswerInline,]
+	extra = 10
+
+
+class QuizAdmin(nested_admin.NestedModelAdmin):
+	"""
+		Setting admin for quiz.
+
+	"""
+	inlines = [QuestionInline,]
+
+
+class PlayersAnswerInline(admin.TabularInline):
+	"""
+		Setting admin for players.
+
+	"""
+	model = PlayersAnswer
+
+
+class SubmitPlayerAdmin(admin.ModelAdmin):
+	"""
+		Configuring admin for player submissions.
+
+	"""
+	inlines = [PlayersAnswerInline,]
+
+# Registers
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(SubmitPlayer, SubmitPlayerAdmin)
+admin.site.register(PlayersAnswer)
