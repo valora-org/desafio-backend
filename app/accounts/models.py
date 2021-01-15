decc360e1803b91394eeab309481b761101adc4d
deilsonmartins <deilsonengeletrica@gmail.com>
2021-01-15 18:13:39 -0300

    criação do models user accounts

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,3 +1,21 @@
+from accounts.managers import CustomUserManager
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+	"""
+		User model to store all the data related to the user.
+		Each user has its username.
+		Can be player or administrator.
+
+	"""
+	username = models.CharField(_('username'), unique=True, max_length=100)
+	is_staff = models.BooleanField(default=False)
+	is_active = models.BooleanField(default=True)
+
+	USERNAME_FIELD = 'username'
+	REQUIRED_FIELDS = []
+
+	objects = CustomUserManager()
