ac7cbf41893ecdebd28a00c2087ccc420676ea08
Diego Marcelino <diego.arm.mar@gmail.com>
2021-01-21 13:50:56 -0300

    fix: remove deprecated reference

--- config/api_router.py
+++ config/api_router.py
@@ -3,14 +3,10 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
-from quiz.users.api.views import UserViewSet
-
 if settings.DEBUG:
     router = DefaultRouter()
 else:
     router = SimpleRouter()
-
-router.register('users', UserViewSet)
 
 app_name = 'api'
 urlpatterns = router.urls
