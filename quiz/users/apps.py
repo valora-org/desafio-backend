5d3aec76414081480f8b550bb9fdf5db7f488033
Diego Marcelino <diego.arm.mar@gmail.com>
2021-01-21 11:48:50 -0300

    refactor: remove signal adapter for users app
    This is not necessary since social media authentication has been
    removed.

--- quiz/users/apps.py
+++ quiz/users/apps.py
@@ -3,11 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class UsersConfig(AppConfig):
+    """App config for users."""
     name = 'quiz.users'
     verbose_name = _('Users')
-
-    def ready(self):
-        try:
-            import quiz.users.signals  # noqa F401
-        except ImportError:
-            pass
