b01d67a9a8ae53919bd8a8caeb0ac65a74ce847d
Diego Marcelino <diego.arm.mar@gmail.com>
2021-01-21 12:38:38 -0300

    feat: create admin for user model

--- quiz/users/admin.py
+++ quiz/users/admin.py
@@ -2,19 +2,13 @@ from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
 
-from quiz.users.forms import UserChangeForm
-from quiz.users.forms import UserCreationForm
-
 User = get_user_model()
 
 
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
+    """Admin page for user model."""
 
-    form = UserChangeForm
-    add_form = UserCreationForm
-    fieldsets = (('User', {
-        'fields': ('name',)
-    }),) + tuple(auth_admin.UserAdmin.fieldsets)
-    list_display = ['username', 'name', 'is_superuser']
-    search_fields = ['name']
+    list_display = ['username', 'name', 'role']
+    list_filter = ['role']
+    search_fields = ['name', 'username', 'role']
