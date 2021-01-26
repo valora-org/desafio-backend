3ab0ac28744bc7500ab670285b2fb2b383b07270
caio de oliveira bezerra <57061719+CaBezOf@users.noreply.github.com>
2021-01-26 11:13:19 -0300

    update api-accounts

---
+++ QuizzBizz/accounts/api.py
@@ -0,0 +1,30 @@
+from accounts.models import User
+from knox.migrations import AuthToken
+from rest_framework import generics
+
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+
+        return Response({
+            'user': UserSerializer(user).data,
+            'token': AuthToken.objects.create(user)[1]
+        })
+
+
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response({
+            'user': UserSerializer(user).data,
+            'token': AuthToken.objects.create(user)[1]
+        })
