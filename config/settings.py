09e23f760237623b401e9ab769608fbb1f26b68e
Diogo Lima <diogo@dr6.com.br>
2021-02-13 14:57:42 -0300

    Default permission

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
 
