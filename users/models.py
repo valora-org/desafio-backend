3239d27564166df2d27390dabbb9cf38f236b5ca
Diogo Lima <diogo@dr6.com.br>
2021-02-13 14:59:28 -0300

    Adicionado tipo do usuário no model

--- users/models.py
+++ users/models.py
@@ -1,4 +1,15 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
-    pass
+    ADMIN = 'admin'
+    PLAYER = 'player'
+
+    TYPE_CHOICES = (
+        (ADMIN, "Admin"),
+        (PLAYER, "Player")
+    )
+
+    type = models.CharField(choices=TYPE_CHOICES, max_length=6,
+                            default=PLAYER)
