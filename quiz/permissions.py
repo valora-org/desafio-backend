bdc5507423476841c441444e2c96c5c5efecc3c3
Diogo Lima <diogo@dr6.com.br>
2021-02-16 00:27:38 -0300

    fix permissions

--- quiz/permissions.py
+++ quiz/permissions.py
@@ -4,6 +4,8 @@ from users.models import User
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
         return request.user.type == User.ADMIN
 
 
