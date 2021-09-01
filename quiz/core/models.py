a1fd8708655b75862c641b5a0c3c9c02ec393de1
dssantos <danilosantos1986@gmail.com>
2021-09-01 16:30:37 -0300

    Creates question model

--- quiz/core/models.py
+++ quiz/core/models.py
@@ -3,3 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     category = models.CharField(max_length=255)
+
+
+class Question(models.Model):
+    question = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
--- quiz/core/tests.py
+++ quiz/core/tests.py
@@ -1,10 +1,24 @@
 from django.test import TestCase
-from .models import Category
+from .models import Category, Question
 
 
 class CategoryModelTest(TestCase):
     def setUp(self):
         self.obj = Category(category='História')
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Category.objects.exists())
+
+
+class QuestionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Category(category='História')
+        self.obj.save()
+        self.obj = Question(
+            question='Q1', 
+            category=Category.objects.all().first()
+            )
         self.obj.save()
 
     def test_create(self):
