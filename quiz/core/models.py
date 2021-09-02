312aac494e0765a55d88c492e013bd17d66e2828
dssantos <danilosantos1986@gmail.com>
2021-09-01 21:16:32 -0300

    Creates answer model

--- quiz/core/models.py
+++ quiz/core/models.py
@@ -8,3 +8,10 @@ class Category(models.Model):
 class Question(models.Model):
     question = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Answer(models.Model):
+    answer = models.CharField(max_length=255)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    right_answer = models.BooleanField(default=False)
--- quiz/core/tests.py
+++ quiz/core/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Category, Question
+from .models import Category, Question, Answer
 
 
 class CategoryModelTest(TestCase):
@@ -22,4 +22,24 @@ class QuestionModelTest(TestCase):
         self.obj.save()
 
     def test_create(self):
-        self.assertTrue(Category.objects.exists())
+        self.assertTrue(Question.objects.exists())
+
+
+class AnswerModelTest(TestCase):
+    def setUp(self):
+        self.obj = Category(category='História')
+        self.obj.save()
+        self.obj = Question(
+            question='Q1', 
+            category=Category.objects.all().first()
+            )
+        self.obj.save()
+        self.obj = Answer(
+            answer = 'A1',
+            question=Question.objects.all().first(), 
+            category=Category.objects.all().first()
+            )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Answer.objects.exists())
