e043a2878243e25e69663909b0fac1e816e1a660
JonathaCnB <jotacarlosftb@gmail.com>
2021-09-05 19:38:56 -0400

    Update: View para listagem de todos os usuarios

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+@api_view(["GET"])
+@permission_classes([IsAdminUser])
+def get_users(request):
+    users = User.objects.filter(is_active=True)
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
