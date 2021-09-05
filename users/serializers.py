9026521b21e61c259752663d4c5b23034409e119
JonathaCnB <jotacarlosftb@gmail.com>
2021-09-05 19:40:19 -0400

    Update: criação de serializer para model users

---
+++ users/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "email", "name", "isAdmin", "score"]
+
+    def get_id(self, obj):
+        return obj.id
+
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == "":
+            name = obj.email
+        return name
