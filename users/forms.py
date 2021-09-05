83a43f1a7c0649441144f08b7a6f69b24de7e03c
JonathaCnB <jotacarlosftb@gmail.com>
2021-09-05 16:24:33 -0400

    Path: implementação de um form para criação e atualização de user

---
+++ users/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import forms
+
+from .models import User
+
+
+class UserChangeForm(forms.UserChangeForm):
+    class Meta(forms.UserChangeForm.Meta):
+        model = User
+
+
+class UserCreationForm(forms.UserCreationForm):
+    class Meta(forms.UserCreationForm.Meta):
+        model = User
