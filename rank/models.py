ceffe3548fcd9e14e2cb609a72f166e66a1c3f3f
JonathaCnB <jotacarlosftb@gmail.com>
2021-09-05 18:49:01 -0400

    Update: criação da app para gerenciar o rank

---
+++ rank/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Rank(models.Model):
+    score = models.PositiveIntegerField(
+        verbose_name="Pontuação",
+        null=True,
+    )
+    category = models.ForeignKey(
+        "questions.Category",
+        verbose_name="Categoria",
+        on_delete=models.PROTECT,
+        null=True,
+    )
+    profile = models.ForeignKey(
+        "users.User",
+        verbose_name="Usuário",
+        on_delete=models.DO_NOTHING,
+    )
+
+    class Meta:
+        verbose_name = "Rank"
+        verbose_name_plural = "Rank"
+        db_table = "rank"
+
+    def __str__(self) -> str:
+        return self.score
