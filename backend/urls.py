627cec6cea11e002b405bc90b734c8aeacc109d8
JonathaCnB <jotacarlosftb@gmail.com>
2021-09-05 23:03:16 -0400

    Update: implementação da rota para app questions

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("api/users/", include("users.urls")),
+    path("api/questions/", include("questions.urls")),
 ]
