aa9cc90e34698ded1ddd867dd32e42aaaf6a9755
JonathaCnB <jotacarlosftb@gmail.com>
2021-09-06 21:27:48 -0400

    Updata: definido rota para model rank

--- backend/urls.py
+++ backend/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("users.urls")),
     path("api/questions/", include("questions.urls")),
+    path("api/rank/", include("rank.urls")),
 ]
