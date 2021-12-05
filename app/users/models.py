a9b4d08c698859214a4e1400e3021246cee6abd3
Gabriel Oliveira <gabriel.oliveira@parafernalia.net.br>
2021-12-05 20:52:40 -0300

    Add missing changes to User model

--- app/users/models.py
+++ app/users/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 
-class User(AbstractBaseUser, models.Model):
+class User(AbstractBaseUser, PermissionsMixin):
 
-    username = models.CharField(max_length=250, blank=True)
+    username = models.CharField(max_length=250, blank=True, unique=True)
     is_admin = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "username"
