dc6e309f2b1e816550db3318b98d32f2c4b2fed4
Gabriel Oliveira <gabriel.oliveira@parafernalia.net.br>
2021-12-05 18:14:42 -0300

    Tweak postgres db connection + add gitignore file

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'categories',
+    'quizzes',
 ]
 
 MIDDLEWARE = [
@@ -77,9 +80,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'NAME': 'postgres',
+        'USER': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
