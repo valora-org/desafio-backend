abb3901856b9ba35947de585d69445fd600b5532
Leandro Medvedev <leandromedvedev@hotmail.com>
2022-11-09 05:33:43 -0300

    chore: python version - django.yml

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv('SECRET_KEY')
+SECRET_KEY = getenv('SECRET_KEY', '0uz+SA0lfkykxR160NCvJJwHUQh3q5Dx+Y52fYyml68=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv('DEBUG', True)
