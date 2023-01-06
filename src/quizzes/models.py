781745f587c5affbc46938c679e9b240bbfaf237
Marcelo Jorge <marcelo.barbosa@supera.com.br>
2023-01-05 22:37:37 -0300

    add quizzes app, migrations and models

---
+++ src/quizzes/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class Category(models.Model):
+    
+    type_text = models.CharField(verbose_name="Tipo da Categoria", max_length=100)
+
+    class Meta:
+        ordering = ("id",)
+        verbose_name = _("Categoria")
+        verbose_name_plural = _("Categorias")
+
+    def __str__(self) -> str:
+        return self.type_text
+
+
+class Answer(models.Model):
+    
+    answer_choices = (
+        ("first_choice", "Primeira escolha"),
+        ("second_choice", "Segunda escolha"),
+        ("third_choice", "Terceira escolha")
+    )
+    
+    category = models.ForeignKey(
+        Category,
+        verbose_name="Categoria",
+        on_delete=models.CASCADE,
+        related_name="anwser"
+    )
+    
+    answer_text = models.CharField(
+        verbose_name="Texto da Pergunta",
+        max_length=200
+    )
+    
+    first_choice = models.CharField(
+        verbose_name="Primeira Escolha",
+        max_length=100
+    )
+    
+    second_choice = models.CharField(
+        verbose_name="Segunda Escolha",
+        max_length=100
+    )
+    
+    third_choice = models.CharField(
+        verbose_name="Terceira Escolha",
+        max_length=100
+    )
+    
+    correct_choice = models.CharField(
+        verbose_name="Escolha correta",
+        max_length=100,
+        choices=answer_choices
+    )
+    
+    class Meta:
+        ordering = ("id",)
+        verbose_name = _("Pergunta")
+        verbose_name_plural = _("Perguntas")
+    
+    def __str__(self) -> str:
+        return self.answer_text
+
---
+++ src/quizzes/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+#from quizzes.views import Teste
+
+urlpatterns = [
+    #path('', Teste.as_view(), name="quizzes")
+]
