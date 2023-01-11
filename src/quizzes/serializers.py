09b1dc306b4bdd5790b0e64d6c3bd560350145ff
Marcelo Jorge <marcelo.barbosa@supera.com.br>
2023-01-11 00:52:29 -0300

    serializers for quizzes endpoints

---
+++ src/quizzes/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from quizzes import models
+
+class GetQuizSerializer(serializers.Serializer):
+    category = serializers.CharField(max_length=100)
+
+
+class PlayQuizSerializer(serializers.Serializer):
+    choices = (
+        ("first_choice", "Primeira escolha"),
+        ("second_choice", "Segunda escolha"),
+        ("third_choice", "Terceira escolha")
+    )
+    answer_choice = serializers.ChoiceField(choices=choices)
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Question
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Category
+        fields = '__all__'
