86a67690de67b30482f8890ffc4e6a3a2c0c8615
Marcelo Jorge <marcelo.barbosa@supera.com.br>
2023-01-11 00:44:08 -0300

    personalized user admin

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,4 +1,48 @@
 from django.contrib import admin
 from accounts.models import User
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-admin.site.register(User)
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ('id',)
+    
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Personal info"),
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "current_quiz",
+                )
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "user_type",
+                )
+            },
+        ),
+    )
+
+    
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+
