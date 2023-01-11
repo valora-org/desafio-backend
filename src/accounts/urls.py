170adb0fd444c64575b7109229f3f070862505fa
Marcelo Jorge <marcelo.barbosa@supera.com.br>
2023-01-11 09:07:49 -0300

    token endpoints

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from django.contrib import admin
 
 urlpatterns = [
-    path('api/token', ObtainAuthToken.as_view(), name="token")
+    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', admin.site.login, name="login")
 ] 
