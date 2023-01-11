5f80be6bb676def79d784600acd2cc4bef86441b
Marcelo Jorge <marcelo.barbosa@supera.com.br>
2023-01-11 00:49:20 -0300

    add play and get quiz endpoints and create question and category

--- src/quizzes/urls.py
+++ src/quizzes/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from quizzes.views import PlayQuizz
+from quizzes import views
 
 urlpatterns = [
-    path('play_quizz', PlayQuizz.as_view(), name="quizzes")
+    path('play_quiz', views.PlayQuiz.as_view(), name="quizzes"),
+    path('get_quiz', views.GetQuiz.as_view(), name="quizzes"),
+    path('create_question', views.CreateQuestion.as_view(), name="quizzes"),
+    path('create_category', views.CreateCategory.as_view(), name="quizzes")
+    
 ] 
