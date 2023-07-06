aed2ef2ed09251191f8496c63e111decf1b3704b
Pedro Augusto <pagb.pedro@hotmail.com>
2023-07-06 16:21:04 -0300

    :wrench: chore: Add configuration to serving static files

--- backendchallenge/settings.py
+++ backendchallenge/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ BASE_DIR / "templates/static",]
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
