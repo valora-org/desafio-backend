bf7e9375a397d7bfe1ba04adda996d14cdc71afb
Pedro Augusto <pagb.pedro@hotmail.com>
2023-07-06 14:09:46 -0300

    :sparkles: feat: Add User endpoint

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from accounts.models import CustomUser as User
+from accounts.serializers import UserSerializer
+from rest_framework import viewsets, permissions
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
