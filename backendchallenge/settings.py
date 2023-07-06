89fd9afbb9e1d42e42d19c2e4864911243252b04
Pedro Augusto <pagb.pedro@hotmail.com>
2023-07-06 14:12:19 -0300

    :wrench: chore: Setting authentication to CustomUser model

--- backendchallenge/settings.py
+++ backendchallenge/settings.py
@@ -107,6 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -134,7 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     
     'DEFAULT_AUTHENTICATION_CLASSES':[
-        'rest_framwork.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
