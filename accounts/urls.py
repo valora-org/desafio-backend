e611f0dbcbc12e76804da4da9c34b31c09650897
Pedro Augusto <pagb.pedro@hotmail.com>
2023-07-06 16:41:14 -0300

    Add logout route

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from django.contrib.auth import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path("login/", views.LoginView.as_view(template_name='login.html'), name='login')
+    path("login/", views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
