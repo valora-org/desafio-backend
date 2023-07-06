1dd18f98c1e61907acb3c453157942aa35d24a09
Pedro Augusto <pagb.pedro@hotmail.com>
2023-07-06 14:16:11 -0300

    :sparkles: feat: Register User to admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from accounts.models import CustomUser as User
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_full_name', 'username', 'score', 'is_staff')
