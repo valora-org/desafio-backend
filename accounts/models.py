b1bc7f8c1f71139172ef76f9ecc2cf8cae19afe6
Pedro Augusto <pagb.pedro@hotmail.com>
2023-07-06 14:20:39 -0300

    :recycle: refactor: Turn model user more flexible

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,53 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 
-class CustomUser(User):
-    
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+
+    username_validator = UnicodeUsernameValidator()
+
+    id = models.AutoField(_("id"), primary_key=True, serialize=False, auto_created=True,)
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
+    first_name = models.CharField(_("first name"), max_length=150, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    email = models.EmailField(_("email address"), blank=True)
     score = models.IntegerField(_("score points"), default=0)
+    is_staff = models.BooleanField(_("staff status"), default=False)
+    is_active = models.BooleanField(_("active"), default=True, )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
     
+    objects = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email", "first_name", "last_name"]
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+        
+
     def __str__(self):
-        return f"{self.first_name.capitalize()} {self.last_name.capitalize()}"
+        return self.get_full_name()
+    
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
