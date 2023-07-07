5166e9a1f3361c2f6de0fad590d04d1cd4546161
Pedro Augusto <pagb.pedro@hotmail.com>
2023-07-07 16:02:24 -0300

    :sparkles: feat: Create a quiz models with relationship between them

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(_("category name"), max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+
+class Quiz(models.Model):
+    name = models.CharField(_("quiz name"), default=_("New Quiz"), max_length=150, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'quiz'
+        verbose_name_plural = 'quizzes'
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Question(models.Model):
+    text = models.CharField(_("question"), max_length=255)
+    quiz = models.ForeignKey(Quiz, on_delete=models.DO_NOTHING)
+    is_active = models.BooleanField(_("active"), default=True)
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+
+
+    class Meta:
+        verbose_name = 'question'
+        verbose_name_plural = 'questions'
+
+    def __str__(self):
+        max_length = 50
+        return f"{self.text[:max_length]}"
+
+class Answer(models.Model):
+    text = models.CharField(_("answer"), max_length=255)
+    question = models.ForeignKey(Question, on_delete=models.DO_NOTHING)
+    is_correct = models.BooleanField(_("correct answer"), default=False)
