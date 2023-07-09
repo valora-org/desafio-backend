1a1e1eea98079bbd5555539689c878a788fe32b0
Pedro Augusto <pagb.pedro@hotmail.com>
2023-07-09 12:43:52 -0300

    :sparkles: feat: Created view wich return one random question of specific quiz

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from quiz.models import Quiz, Question, Answer
+from quiz.serializers import QuizSerializer, QuestionSerializer
+import random
 
-# Create your views here.
+
+class QuizView(viewsets.ModelViewSet):
+    serializer_class = QuizSerializer
+    queryset = Quiz.objects.all()
+
+    def retrieve(self, request, format=None, pk=None):
+        """
+        Return one random question when quiz are called
+        """
+        question = Question.objects.filter(quiz__pk=pk).order_by('?')[:1]
+        serializer = QuestionSerializer(question, context={'request': request}, many=True)
+        return Response(serializer.data)
