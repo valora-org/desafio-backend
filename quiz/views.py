23a2c2f8db72e24b1cf069b611171213d20d607e
Pedro Augusto <pagb.pedro@hotmail.com>
2023-07-10 16:06:17 -0300

    :sparkles: feat: Adding permissions to api endpoint

--- quiz/views.py
+++ quiz/views.py
@@ -1,13 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from quiz.models import Quiz, Question, Answer
 from quiz.serializers import QuizSerializer, QuestionSerializer
-import random
+from django.core.exceptions import PermissionDenied
 
 
+   
 class QuizView(viewsets.ModelViewSet):
     serializer_class = QuizSerializer
     queryset = Quiz.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
     def retrieve(self, request, format=None, pk=None):
         """
@@ -16,3 +18,23 @@ class QuizView(viewsets.ModelViewSet):
         question = Question.objects.filter(quiz__pk=pk).order_by('?')[:1]
         serializer = QuestionSerializer(question, context={'request': request}, many=True)
         return Response(serializer.data)
+    
+
+    def create(self, request, *args, **kwargs):
+        if request.user.is_staff:
+            return super().create(request, *args, **kwargs)
+        else:
+            raise PermissionDenied()
+        
+    def update(self, request, *args, **kwargs):
+        if request.user.is_staff:
+            return super().update(request, *args, **kwargs)
+        else:
+            raise PermissionDenied()
+        
+    def destroy(self, request, *args, **kwargs):
+        if request.user.is_staff:
+            return super().destroy(request, *args, **kwargs)
+        else:
+            raise PermissionDenied()
+    
