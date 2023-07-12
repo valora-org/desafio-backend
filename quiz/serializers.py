733aa5a6fca310003366304fdc4330d7f56e2bcc
Pedro Augusto <pagb.pedro@hotmail.com>
2023-07-12 13:20:21 -0300

    :sparkles: feat: Add url in answer serialization

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -24,6 +24,7 @@ class AnswerSerializer(serializers.ModelSerializer):
         fields = [
             
             'id',
+            'url',
             'question',
             'text', 
             'is_correct',
