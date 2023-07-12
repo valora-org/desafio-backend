d850c4eb53aed5812b846d337bd9a788ecd6a6a5
Pedro Augusto <pagb.pedro@hotmail.com>
2023-07-12 10:38:52 -0300

    :sparkles: feat: Adding API pagination

--- backendchallenge/settings.py
+++ backendchallenge/settings.py
@@ -141,6 +141,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    
+    'PAGE_SIZE': 3,
     
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.TokenAuthentication',
